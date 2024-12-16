af03f5e1f8b04dfdfbdf5e10cb67e6930bf20fde
Swapnil Mandade <Swapnil Mandade@TSLPUN180.tslpune.local>
2024-12-16 14:54:12 +0530

    16dec

--- EmailVerification-Project/src/main/java/com/tsl/controller/EmailVerificationController.java
+++ EmailVerification-Project/src/main/java/com/tsl/controller/EmailVerificationController.java
@@ -14,6 +14,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import java.util.List;
 
 @Controller
+//first_check
 @RequestMapping("/email-verification")
 public class EmailVerificationController {
 
